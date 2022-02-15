4fbc6d3065862906f14ac9b63f717d8392a07106
Ivan Torres Garcia <ivantorresgarcia.02@gmail.com>
2022-02-15 22:28:37 +0100

    feat(#1):Setup project
    Base structure

---
+++ src/models/Book.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+import { Author, AuthorSchema } from './Author'
+import { CommentSchema, Comment } from './Comment'
+
+//! INTERFACES
+//* Exporting the interface, we can use it everywhere
+interface Book extends mongoose.Document {
+  title: String
+  editorial: String
+  price: Number
+  author: Author
+  comments: Comment[]
+}
+
+//! SCHEMA
+const BookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 3,
+    trim: true,
+  },
+  editorial: {
+    type: String,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    trim: true,
+    min: 0,
+  },
+  author: AuthorSchema, //! Para subdocumentos hay que usar el Schema apropiado
+  comments: [CommentSchema],
+})
+
+//* Using the Schema, we create a MongoDB Collection called 'books' which will follow the requirements
+let BookModel = mongoose.model<Book>('books', BookSchema)
+export { Book, BookSchema, BookModel }
